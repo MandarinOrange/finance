ffed30d29186e6323b52ea5d70e00f75e4237038
jyx <1584081113@qq.com>
2019-01-06 21:26:12 +0800

    修改了类名以及添加了bean和dao类

--- demo/src/test/java/com/example/demo/Dao/userMapper.java
+++ demo/src/test/java/com/example/demo/Dao/userMapper.java
@@ -1,6 +1,6 @@
 package com.example.demo.Dao;
 
-import com.example.demo.bean.user;
+import com.example.demo.bean.User;
 import org.apache.ibatis.annotations.*;
 
 import java.util.List;
@@ -8,22 +8,22 @@ import java.util.List;
 @Mapper
 public interface userMapper {
     @Select("select * from user where userNum = #{userNum}")
-    public user selectUserByuserNum(String userNum);
+    public User selectUserByuserNum(String userNum);
 
     @Select("select * from user")
-    public List<user> select();
+    public List<User> select();
 
     @Insert("insert into user(userNum, userName, userPwd, userRight," +
             " tureName, idNum, telephoneNum, address, isIdentify, registerTime)" +
             " values (#{userNum},#{userName},#{userPwd},#{userRight},#{tureName}," +
             "#{idNum},#{telephoneNum},#{address},#{isIdentify},#{registerTime})")
-    public void addUser(user users);
+    public void addUser(User users);
 
     @Update("update user set userNum=#{userNum},userName=#{userName}" +
             ",userPwd=#{userPwd},userRight=#{userRight},tureName=#{tureName}," +
             "idNum=#{idNum},telephoneNum=#{telephoneNum},address=#{address}," +
             "isIdentify=#{isIdentify},registerTime=#{registerTime}where userNum=#{userNum}")
-    public void updateUser(user users);
+    public void updateUser(User users);
 
     @Delete("delete from user where userNum=#{userNum}")
     public void deleteUser(String userNum);

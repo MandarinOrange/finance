649c00f275a91586ff884efc30c145fd39c16d15
jyx <1584081113@qq.com>
2019-01-16 16:26:27 +0800

    认证界面

--- demo/src/main/java/com/example/demo/circulator/controller/PayCirController.java
+++ demo/src/main/java/com/example/demo/circulator/controller/PayCirController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,7 +21,7 @@ public class PayCirController {
     PayCirService payCirService;
 
     @PostMapping("/pay")
-    public void payAmountCir(HttpServletRequest request, HttpServletResponse response){
+    public void payAmountCir(HttpServletRequest request, HttpServletResponse response)throws IOException {
         long checkNum = Long.parseLong(request.getParameter("checkNum"));
         double pay = Double.parseDouble(request.getParameter("amount"));
         HttpSession session = request.getSession();
@@ -30,8 +31,8 @@ public class PayCirController {
         Map map = new HashMap();
         map.put("value",result);
         if(result==1){
-
             String json = JSONObject.toJSONString(map);
+            response.getWriter().print(json);
         }else if(result==-2){
             //return"/未贷款该业务!";
         }else if(result==-3){
--- demo/src/main/java/com/example/demo/register/controller/bankCardController.java
+++ demo/src/main/java/com/example/demo/register/controller/bankCardController.java
@@ -9,6 +9,7 @@ import com.example.demo.register.identify;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,7 +19,8 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
+//@Controller
+@RestController
 public class bankCardController extends HttpServlet {
 
     @Autowired

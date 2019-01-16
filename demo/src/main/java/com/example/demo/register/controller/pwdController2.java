a3fd02acef9e007aa4f28e03d3d620c944add57f
Aokiso <1010390029@qq.com>
2019-01-16 10:49:51 +0800

    修改

---
+++ demo/src/main/java/com/example/demo/register/controller/pwdController2.java
@@ -0,0 +1,33 @@
+package com.example.demo.register.controller;
+
+import com.example.demo.Dao.userMapper;
+import com.example.demo.bean.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@Controller
+public class pwdController2 extends HttpServlet {
+    @Autowired
+    userMapper userMapper;
+
+    @RequestMapping("/pwd2")//再一次确认密码界面
+    public void doPost(HttpServletResponse response, HttpServletRequest request)throws IOException {
+        HttpSession session=request.getSession();
+        User user=new User();
+        user=(User)session.getAttribute("user");
+        String pwd=request.getParameter("userPwd");
+        if(userMapper.selectPwd(user.getUserNum(),pwd)==1){
+            response.sendRedirect("loanInformation.html");//贷款的界面
+        }else{
+            response.sendRedirect("Untitled-1.html");//登陆失败
+        }
+    }
+
+}
